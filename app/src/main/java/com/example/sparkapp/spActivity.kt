2df6379370c35cfd0e34722c18839a914ea0212f
Tanmay Rajawat <77978645+Tanmayrajawat@users.noreply.github.com>
2021-09-20 22:04:47 +0530

    first commit

---
+++ app/src/main/java/com/example/sparkapp/spActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sparkapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.airbnb.lottie.LottieAnimationView
+
+class spActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long=3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sp)
+        val lottieAnimation = findViewById<LottieAnimationView>(R.id.lottie)
+        lottieAnimation.animate().translationY(1400f).setDuration(1000).setStartDelay(4000)
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
